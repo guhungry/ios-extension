22b08ddc2cd8e14f0e8fe669b18a12a857e95846
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2018-09-03 14:03:50 +0700

    [Git] Initial Import

---
+++ WCExtensions/WCExtensions.h
@@ -0,0 +1,19 @@
+//
+//  WCExtensions.h
+//  WCExtensions
+//
+//  Created by Woraphot Chokratanasombat on 3/9/2561 BE.
+//  Copyright © 2561 Woraphot Chokratanasombat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WCExtensions.
+FOUNDATION_EXPORT double WCExtensionsVersionNumber;
+
+//! Project version string for WCExtensions.
+FOUNDATION_EXPORT const unsigned char WCExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCExtensions/PublicHeader.h>
+
+
