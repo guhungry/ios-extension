73435fa255a050f94aa7a8998d0a701489e339a0
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-09-03 23:14:25 +0700

    [Regex Tests] Change: Test for invalid regex syntax

--- WCExtensionsTests/Wrappers/Regex.Tests.swift
+++ WCExtensionsTests/Wrappers/Regex.Tests.swift
@@ -12,23 +12,28 @@ import Hamcrest
 
 class RegexTest: XCTestCase {
     var sut: Regex!
-    
+
     override func setUp() {
         super.setUp()
         sut = Regex("\\d+")
     }
-    
+
     override func tearDown() {
         sut = nil
-        
+
         super.tearDown()
     }
-    
+
     func testMatchesWithDPLUS_WhenTextISAbc_ShouldEmpty() {
         assertThat(sut.matches("Abc"), hasCount(0))
     }
-    
+
     func testMatchesWithDPLUS_WhenTextIS123_ShouldHave1Match() {
         assertThat(sut.matches("123"), hasCount(1))
+    }
+
+    func testMatches_WhenInvalidRegex_ShouldNotErrorAndNotMatch() {
+        sut = Regex("(InvalidRegexSyntax")
+        assertThat(sut.matches("123"), hasCount(0))
     }
 }
