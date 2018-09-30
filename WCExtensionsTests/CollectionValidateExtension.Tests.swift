cfeeb7700a170a401212d15b334e4b8ad74166b8
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-10-01 01:06:03 +0700

    [CollectionValidateExtensionTests] Change: Rename file to CollectionValidateExtension.Tests.swift

--- WCExtensionsTests/CollectionValidateExtension.Tests.swift
+++ WCExtensionsTests/CollectionValidateExtension.Tests.swift
@@ -10,7 +10,7 @@ import XCTest
 import Hamcrest
 @testable import WCExtensions
 
-class CollectionValidateExtensionTest: XCTestCase {
+class CollectionValidateExtensionTests: XCTestCase {
     /////////
     /// any()
     /////////
