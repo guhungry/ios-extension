9e75f39d203f7970ff0ec6c9cf9d9548e1793319
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-10-07 01:01:12 +0700

    [Collection Extension@reduceIndexed()] Change: Don't destruct tuple anymore

--- WCExtensions/CollectionExtension.swift
+++ WCExtensions/CollectionExtension.swift
@@ -51,10 +51,7 @@ public extension Collection {
     }
 
     public func reduceIndexed<R>(initial: R, reducer: (R, Int, Element) -> R) -> R {
-        return enumerated().reduce(initial) { acc, element in
-            let (offset, element) = element
-            return reducer(acc, offset, element)
-        }
+        return enumerated().reduce(initial) { acc, it in reducer(acc, it.offset, it.element) }
     }
 }
 
